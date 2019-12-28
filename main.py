3e197ed5130a870e24e7420ded18b53e60c031fa
mtsml <50922910+mtsml@users.noreply.github.com>
2019-12-28 16:41:59 +0900

    indexページ作成

--- main.py
+++ main.py
@@ -37,9 +37,15 @@ def callback():
 
     return 'OK'
 
+@app.route("/index", methods=['POST'])
+def index():
+    return 'Hello World!'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event.source.user_id)
+    app.logger.info(event.source.user_id)
+
     if '好き' in event.message.text:
         text='私も好き'
     else:
