638f03181aedc86495080ec3b118657a45647c99
matsu <50922910+mtsml@users.noreply.github.com>
2020-07-24 15:22:18 +0900

    [Mod] コロナの取得元を修正

--- tests/test_util.py
+++ tests/test_util.py
@@ -16,15 +16,66 @@ class TestUtil(unittest.TestCase):
         soup = BeautifulSoup(features="html.parser")
         self.assertEqual(type(res), type(soup))
 
-    @patch('dokipro1.util.get_soup_by_url')
-    def test_get_covid19_info(self, mock_func):
-        text = '''
-            <span class="DataView-DataInfo-summary">72</span>
-            <small class="DataView-DataInfo-date">4/26 実績値（前日比: -31 人）</small>
-        '''
-        soup = BeautifulSoup(text, "html.parser")
-        mock_func.return_value = soup
-        expected = '本日の東京都のコロナ陽性患者数は72人です。\n4/26 実績値（前日比: -31 人）'
+    @patch('requests.get')
+    def test_get_covid19_info_plus(self, mock_func):
+        json = [
+            {
+                "diagnosed_date": "2020-04-25",
+                "count": 119,
+                "missing_count": 68,
+                "reported_count": 51,
+                "weekly_gain_ratio": 0.8,
+                "untracked_percent": 60.6,
+                "weekly_average_count": 125.6,
+                "weekly_average_untracked_count": 76.1,
+                "weekly_average_untracked_increse_percent": 74.1
+            },
+            {
+                "diagnosed_date": "2020-04-26",
+                "count": 82,
+                "missing_count": 56,
+                "reported_count": 26,
+                "weekly_gain_ratio": 0.82,
+                "untracked_percent": 61.3,
+                "weekly_average_count": 121.7,
+                "weekly_average_untracked_count": 74.6,
+                "weekly_average_untracked_increse_percent": 73
+            }
+        ]
+
+        mock_func.return_value = json
+        expected = '本日の東京都のコロナ陽性患者数は82人です。\n（前日比: -37 人）'
+        self.assertEqual(util.get_covid19_info(const.TODAY), expected)
+
+    @patch('requests.get')
+    def test_get_covid19_info_minus(self, mock_func):
+        json = [
+            {
+                "diagnosed_date": "2020-07-22",
+                "count": 238,
+                "missing_count": 138,
+                "reported_count": 100,
+                "weekly_gain_ratio": 1.3,
+                "untracked_percent": 53.4,
+                "weekly_average_count": 242.9,
+                "weekly_average_untracked_count": 129.6,
+                "weekly_average_untracked_increse_percent": 148.2
+            },
+            {
+                "diagnosed_date": "2020-07-23",
+                "count": 366,
+                "missing_count": 225,
+                "reported_count": 141,
+                "weekly_gain_ratio": 1.3,
+                "untracked_percent": 55.9,
+                "weekly_average_count": 254.3,
+                "weekly_average_untracked_count": 142.1,
+                "weekly_average_untracked_increse_percent": 154
+            }
+        ]
+
+        mock_func.return_value = json
+        expected = '本日の東京都のコロナ陽性患者数は366人です。\n（前日比: +128 人）'
         self.assertEqual(util.get_covid19_info(const.TODAY), expected)
 
     @patch('dokipro1.util.get_soup_by_url')
