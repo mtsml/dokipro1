79d130d455efdbfa692679f8fb6e45efca123c1b
matsu <50922910+mtsml@users.noreply.github.com>
2021-06-03 23:02:40 +0900

    jsonファイルを絶対パスで指定するように変更

--- dokipro1/keiba.py
+++ dokipro1/keiba.py
@@ -1,8 +1,12 @@
 import json
-
+import os
 
 def get_race_choice_message():
-    data = open('assets/keiba_choice.json', mode='r')
+    dirname = os.getcwd()
+    print(dirname)
+    path = os.path.join(dirname, 'dokipro1/assets/keiba_choice.json')
+    print(path)
+    data = open(path, mode='r')
     message = json.load(data)
     data.close()
     return message
