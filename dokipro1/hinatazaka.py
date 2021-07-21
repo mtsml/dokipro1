6091ebfb263df197c237ec584fff6720ebd402ba
matsu <50922910+mtsml@users.noreply.github.com>
2021-07-21 23:31:59 +0900

    [日向坂] 記事へ遷移するボタンを追加

--- dokipro1/hinatazaka.py
+++ dokipro1/hinatazaka.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 import dokipro1.util as util
 
+
+HINATAZAKA_HOST = 'https://www.hinatazaka46.com'
 HINATAZAKA_URL = 'https://www.hinatazaka46.com/s/official/diary/member/list?ima=0000&dy={}'
 
 
@@ -15,6 +17,7 @@ def get_article_list():
 
     return article_list
 
+
 def build_hinatazaka_json():
     base_path = 'dokipro1/assets/hinatazaka_base.json'
     bubble_path = 'dokipro1/assets/hinatazaka_bubble.json'
@@ -22,11 +25,13 @@ def build_hinatazaka_json():
     for article in get_article_list():
         bubble_json = util.get_json(bubble_path)
         image_url = article.find('img').get('src')
+        article_url = HINATAZAKA_HOST + article.find('a', class_='c-button-blog-detail').get('href')
 
         bubble_json["hero"]["url"] = image_url
         bubble_json["body"]["contents"][0]["text"] = article.find('div', class_='c-blog-article__title').string.strip()
         bubble_json["body"]["contents"][1]["contents"][0]["text"] = article.find('div', class_='c-blog-article__name').string.strip()
-        bubble_json["footer"]["contents"][0]["action"]["uri"] = image_url
+        bubble_json["footer"]["contents"][0]["action"]["uri"] = article_url
+        bubble_json["footer"]["contents"][1]["action"]["uri"] = image_url
 
         json["contents"].append(bubble_json)
 
