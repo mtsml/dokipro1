5d478d4c4d2e27a7a0dafca333384c2b03f68d4f
tmg26 <59874692+tmg26@users.noreply.github.com>
2021-07-23 12:57:38 +0900

    日向坂ブログに投稿された画像の生存確認を行うよう修正

--- dokipro1/hinatazaka.py
+++ dokipro1/hinatazaka.py
@@ -7,12 +7,13 @@ import dokipro1.util as util
 
 HINATAZAKA_HOST = 'https://www.hinatazaka46.com'
 HINATAZAKA_URL = 'https://www.hinatazaka46.com/s/official/diary/member/list?ima=0000&dy={}'
+HINATAZAKA_IMAGE_HOST = 'https://cdn.hinatazaka46.com'
+HINATAZAKA_OFFICIAL_LOGO = 'https://cdn.hinatazaka46.com/files/14/hinata/img/logo_side.svg'
 
 
 def get_article_list():
     yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y%m%d')
-    res = requests.get(HINATAZAKA_URL.format(yesterday))
-    soup = BeautifulSoup(res.text, 'html.parser')
+    soup = get_article(HINATAZAKA_URL.format(yesterday))
     article_list = soup.find_all('div', class_='p-blog-article')
 
     return article_list
@@ -25,6 +26,8 @@ def build_hinatazaka_json():
     for article in get_article_list():
         bubble_json = util.get_json(bubble_path)
         image_url = article.find('img').get('src')
+        image_url = HINATAZAKA_OFFICIAL_LOGO if not is_image_url_alive(image_url)
+
         article_url = HINATAZAKA_HOST + article.find('a', class_='c-button-blog-detail').get('href')
 
         bubble_json["hero"]["url"] = image_url
@@ -46,9 +49,21 @@ def to_short_text(article):
 
 
 def get_image_url_list(article_url):
-    res = requests.get(article_url)
-    soup = BeautifulSoup(res.text, 'html.parser')
+    soup = get_article(article_url)
     article = soup.find('div', class_='p-blog-article')
     image_list = article.find_all('img')
-    image_url_list = [image.get('src') for image in image_list]
+    all_image_url_list = [image.get('src') for image in image_list]
+    image_url_list = []
+    for image_url in all_image_url_list:
+        image_url_list.append(image_url) if is_image_url_alive(image_url)
+
     return image_url_list
+
+
+def get_article(article_url):
+    res = requests.get(article_url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    return soup
+
+def is_image_url_alive(image_url):
+    return image_url.startswith(HINATAZAKA_IMAGE_HOST)
